a07ab64cebf557d3605f20dd4dabc16a9b2056b5
UKUNDERGROUNDNETWORK <UKUNDERGROUNDNETWORK@gmail.com>
2026-07-08 03:25:07 +0100

    Instant push

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8788'
+  build: {
+    rollupOptions: {
+      input: 'template.html'   // use renamed template
     }
   }
 });
